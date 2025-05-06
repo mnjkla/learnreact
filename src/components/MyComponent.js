f2a919b4240a6d360f8f4ca441266855ff71200c
mnjkla <minhyukiyudai@gmail.com>
2025-05-06 22:28:48 +0700

    video 17

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,15 +1,17 @@
 import React from "react";
 class MyComponent extends React.Component {
+  state = {
+    name: "Minh",
+    age: 20,
+  };
+  //jsx
   render() {
     return (
       <div>
         <h1>Hello, World!</h1>
         <p>This is my first React component.</p>
-        {Math.random() > 0.5 ? (
-          <p>Random number is greater than 0.5</p>
-        ) : (
-          <p>Random number is less than or equal to 0.5</p>
-        )}
+        <p>Name: {this.state.name}</p>
+        <p>Age: {this.state.age}</p>
       </div>
     );
   }
