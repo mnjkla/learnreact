37496d141d8576736fedc0a3e1b8460293bc28fa
mnjkla <minhyukiyudai@gmail.com>
2025-05-08 15:03:51 +0700

    vid 19

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,14 +4,25 @@ class MyComponent extends React.Component {
     name: "Minh",
     age: 20,
   };
+  handleClick(event) {
+    console.log("Hello, World!");
+    // console.log(event.target);
+    console.log("my name is " + this.state.name);
+  }
+  handleOnMoverOver(event) {
+    console.log(event.pageX);
+  }
+
   //jsx
   render() {
     return (
       <div>
         <h1>Hello, World!</h1>
         <p>This is my first React component.</p>
-        <p>Name: {this.state.name}</p>
+        <p>Name: {this.state.name} </p>
         <p>Age: {this.state.age}</p>
+        <button onMouseOver={this.handleOnMoverOver}>Hover me</button>
+        <button onClick={this.handleClick}> click me</button>
       </div>
     );
   }
