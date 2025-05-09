eb960077d9c558194613ecee98d056b5ab72265b
mnjkla <minhyukiyudai@gmail.com>
2025-05-09 23:15:50 +0700

    vid 24

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,18 +2,40 @@ import React from "react";
 
 class DisplayInfor extends React.Component {
   render() {
-    console.log(this.props);
-    const { myInfor } = this.props;
+    const { listUser } = this.props;
     // Destructuring props to get myInfor
     // const { name, age } = this.props;
     return (
       <div>
-        <h2>Display Information</h2>
+        {listUser.map((x) => {
+          return (
+            <div key={x.id}>
+              <h2>Display Information</h2>
+              <p>Name: {x.name}</p>
+              <p>Age: {x.age}</p>
+              <p>Address: {x.address}</p>
+              <hr />
+            </div>
+          );
+        })}
+        {/* {<h2>Display Information</h2>
         <p>Name: {this.props.name}</p>
         <p>Age: {this.props.age}</p>
         <p>
           Address: {this.props.myInfor ? this.props.myInfor.address : "N/A"}
         </p>
+        <hr />
+        <p>Name: {this.props.name}</p>
+        <p>Age: {this.props.age}</p>
+        <p>
+          Address: {this.props.myInfor ? this.props.myInfor.address : "N/A"}
+        </p>{" "}
+        <hr />
+        <p>Name: {this.props.name}</p>
+        <p>Age: {this.props.age}</p>
+        <p>
+          Address: {this.props.myInfor ? this.props.myInfor.address : "N/A"}
+        </p>} */}
       </div>
     );
   }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,15 +2,23 @@ import React from "react";
 import UserInfor from "./UserInfor";
 import DisplayInfor from "./DisplayInfor";
 class MyComponent extends React.Component {
+  state = {
+    listUser: [
+      { id: 1, name: "Minh Minh", age: 30, address: "Hà Nội" },
+      { id: 2, name: "a a", age: 30, address: "Hà Nội" },
+      { id: 3, name: "e ee", age: 30, address: "Hà Nội" },
+    ],
+  };
   //jsx
   render() {
-    const myInfor = { age: 30, name: "Minh Minh", address: "Ha Noi" };
+    //DRY
     return (
       <div>
         <UserInfor />
-        <DisplayInfor name="Minh Minh" age="30" />
+        <br />
+        <br />
+        <DisplayInfor listUser={this.state.listUser} />
         <hr />
-        <DisplayInfor name="Minh   ecMinh" age="30" myInfor={myInfor} />
       </div>
     );
   }
